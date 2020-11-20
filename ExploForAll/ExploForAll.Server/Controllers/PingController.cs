db72e70100eda6bb2359b676e706967a8cb9f656
GreaseMonkey <michaeljonker+github@outlook.com>
2020-11-20 09:33:37 +0100

    Ping

---
+++ ExploForAll/ExploForAll.Server/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ExploForAll.Server.Controllers
+{
+    public class PingController : ControllerBase
+    {
+        [Route("/Ping")]
+        public async Task<string> Ping()
+        {
+            return "Pong";
+        }
+    }
+}
