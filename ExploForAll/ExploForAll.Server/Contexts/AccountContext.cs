bee8e397e9ad39948f00ffef244475cc159a85ee
GreaseMonkey <michaeljonker+github@outlook.com>
2020-11-20 09:47:10 +0100

    Created user info

--- ExploForAll/ExploForAll.Server/Contexts/AccountContext.cs
+++ ExploForAll/ExploForAll.Server/Contexts/AccountContext.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using ExploForAll.Server.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ExploForAll.Server.Contexts
 {
-    public class AccountContext
+    public class AccountContext : IdentityDbContext<Account>
     {
+        public AccountContext(DbContextOptions<AccountContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
--- ExploForAll/ExploForAll.Server/Models/Account.cs
+++ ExploForAll/ExploForAll.Server/Models/Account.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace ExploForAll.Server.Models
 {
-    public class Account
+    public class Account : IdentityUser
     {
     }
 }
