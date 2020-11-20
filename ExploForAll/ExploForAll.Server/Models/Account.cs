5a6a415a47c691b8595c25389982dfaf5aea5721
GreaseMonkey <michaeljonker+github@outlook.com>
2020-11-20 09:42:42 +0100

    Added account stuff

---
+++ ExploForAll/ExploForAll.Server/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploForAll.Server.Models
+{
+    public class Account
+    {
+    }
+}
