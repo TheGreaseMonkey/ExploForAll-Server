76e750cf69f7a143368887353aecaae4c3b1e26e
GreaseMonkey <michaeljonker+github@outlook.com>
2020-11-21 19:49:39 +0100

    Added use case for logging in

---
+++ ExploForAll/ExploForAll.Server/Interactors/AccountUseCase/Queries/LoginToAccount/LoginToAccountHandler.cs
@@ -0,0 +1,26 @@
+﻿using ExploForAll.Server.Models;
+using ExploForAll.Server.Models.Account;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExploForAll.Server.Interactors.AccountUseCase.Queries.LoginToAccount
+{
+    public class LoginToAccountHandler : IRequestHandler<LoginToAccountRequest, Response>
+    {
+        private readonly UserManager<Account> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public LoginToAccountHandler(UserManager<Account> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<Response> Handle(LoginToAccountRequest request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ExploForAll/ExploForAll.Server/Interactors/AccountUseCase/Queries/LoginToAccount/LoginToAccountRequest.cs
@@ -0,0 +1,15 @@
+﻿using ExploForAll.Server.Models;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExploForAll.Server.Interactors.AccountUseCase.Queries.LoginToAccount
+{
+    public class LoginToAccountRequest : IRequest<Response>
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        public string Password { get; set; }
+    }
+}
