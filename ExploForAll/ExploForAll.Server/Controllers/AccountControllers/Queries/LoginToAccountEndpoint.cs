01428ad8df2a620c897c0643451794fda941408b
GreaseMonkey <michaeljonker+github@outlook.com>
2020-11-22 08:46:53 +0100

    Added endpoint /account/login for login use case

---
+++ ExploForAll/ExploForAll.Server/Controllers/AccountControllers/Queries/LoginToAccountEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using ExploForAll.Server.Interactors.AccountUseCase.Queries.LoginToAccount;
+using ExploForAll.Server.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploForAll.Server.Controllers.AccountControllers.Queries
+{
+    [ApiController]
+    public class LoginToAccountEndpoint : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LoginToAccountEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("/Account/Login")]
+        public async Task<IActionResult> LoginToAccount([FromBody] LoginToAccountRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Response response = await _mediator.Send(request);
+
+            return StatusCode((int)response.Status, response);
+
+        }
+
+    }
+}
